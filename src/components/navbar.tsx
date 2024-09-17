82dd7de2b48bfd6a6e0d68633490dea38e8e5279
Anna <32278291+annasudol@users.noreply.github.com>
2024-09-17 08:54:11 +0200

    nav updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,101 +1,15 @@
-// export const Navbar = () => {
-//   return (
-//     <nav className="sticky top-0 sm:static bg-base-200 sm:bg-transparent flex items-center justify-between w-full !h-[64px] !min-h-[64px]">
-//       <div className="flex h-full">
-//         <img src="/public/vite.svg" alt="logo" className='w-32 sm:w-52' />
-//       </div>
-//       <div className="flex items-center content-center h-full relative">
-//         <div className="flex pr-4">
-//           <radix-connect-button></radix-connect-button>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-import { Kbd } from '@nextui-org/kbd';
 import { Link } from '@nextui-org/link';
-import { Input } from '@nextui-org/input';
-import {
-  Navbar as NextUINavbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from '@nextui-org/navbar';
-import { link as linkStyles } from '@nextui-org/theme';
-import clsx from 'clsx';
-
-import { siteConfig } from '@/config/site';
-
 export const Navbar = () => {
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" className='bg-mainDark'>
-      <NavbarContent className="basis-2/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
-          <Link className="w-32 sm:w-52" color="foreground" href="/">
-            <img src="/logo.svg" alt="logo" className="w-32 sm:w-52" />
-          </Link>
-        </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: 'foreground' }),
-                  'data-[active=true]:text-primary data-[active=true]:font-medium',
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-      </NavbarContent>
-      {/* <NavbarContent className="flex basis-1/5 sm:basis-full" justify="end">
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-    
-      </NavbarContent> */}
+    <nav className="sticky top-0  flex items-center justify-between container">
+      <Link className="w-32 sm:w-52" color="foreground" href="/">
+        <img src="/logo.svg" alt="logo" className="w-32 sm:w-52" />
+      </Link>
 
-      <NavbarContent className=" basis-1 pl-4" justify="end">
-        <div id="connect-btn">
-          <radix-connect-button />
-        </div>
-        <div className="sm:hidden">
-          <NavbarMenuToggle />
-        </div>
-      </NavbarContent>
-
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={index === 2 ? 'primary' : index === siteConfig.navMenuItems.length - 1 ? 'danger' : 'foreground'}
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
+      <div id="connect-btn">
+        <radix-connect-button />
+      </div>
+    </nav>
   );
 };
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -15,20 +15,22 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
     // }, []);
   return (
     <Provider store={store}>
-      <div className="relative flex flex-col h-screen">
-        <Navbar />
-        <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">{children}</main>
-        <footer className="w-full flex items-center justify-center py-3">
-          <Link
-            isExternal
-            className="flex items-center gap-1 text-current"
-            href="https://www.radixdlt.com/"
-            title="radixdlt.com/ homepage"
-          >
-            <span className="text-default-600">Powered with</span>
-            <p className="text-primary">Radix</p>
-          </Link>
-        </footer>
+      <div className="h-screen prose md:prose-lg lg:prose-xl max-w-none flex flex-col">
+        <div className="flex flex-col items-center justify-between h-screen w-screen overflow-x-hidden">
+          <Navbar />
+          <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">{children}</main>
+          <footer className="w-full flex items-center justify-center py-3">
+            <Link
+              isExternal
+              className="flex items-center gap-1 text-current"
+              href="https://www.radixdlt.com/"
+              title="radixdlt.com/ homepage"
+            >
+              <span className="text-default-600">Powered with</span>
+              <p className="text-primary">Radix</p>
+            </Link>
+          </footer>
+        </div>
       </div>
     </Provider>
   );
